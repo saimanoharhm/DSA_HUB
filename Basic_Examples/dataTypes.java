32f5cd077353feb9e38c3d9a5554b1252d71966f
Annam Sai Manohar <saimanohar987@gmail.com>
2022-01-28 08:28:10 +0530

    feat: Add Today's Work!

---
+++ Basic_Examples/dataTypes.java
@@ -0,0 +1,11 @@
+public class dataTypes {
+  public static void main(String[] args) {
+    boolean isValid = true;
+    byte marks = 50;
+    float PI = 3.14f;
+    float div = 15.0f / 4.0f;
+    long views = 1000000000;
+    char gender = 'G';
+    System.out.println(isValid+"\n"+marks+"\n"+PI+"\n"+div+"\n"+views+"\n"+gender);
+  }
+}
---
+++ Basic_Examples/swapTwoNumbers.java
@@ -0,0 +1,11 @@
+public class swapTwoNumbers {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int temp = 0;
+    temp = a;
+    a = b;
+    b = temp;
+    System.out.println(a+" "+b);
+  }
+}
